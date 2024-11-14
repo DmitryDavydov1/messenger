c337e6d3287a56923cd5f07caca6a23874901280
имя фамилия <Dima>
2024-11-14 04:28:19 +0300

    add Swagger

--- src/main/java/com/example/simplemessenger/SimpleMessengerApplication.java
+++ src/main/java/com/example/simplemessenger/SimpleMessengerApplication.java
@@ -1,9 +1,11 @@
 package com.example.simplemessenger;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition
 public class SimpleMessengerApplication {
 
 	public static void main(String[] args) {
